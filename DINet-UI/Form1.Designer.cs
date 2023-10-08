2c614c1549e73aecd7320bf0da1aa332313d97e2
natlamir007 <natlamir007@gmail.com>
2023-10-07 23:30:41 -0500

    Add project files.

---
+++ DINet-UI/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace DINet_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInputVideo = new System.Windows.Forms.TextBox();
+            this.btnBrowseInputVideo = new System.Windows.Forms.Button();
+            this.btnBrowseInputAudio = new System.Windows.Forms.Button();
+            this.txtInputAudio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtShellOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Video:";
+            // 
+            // txtInputVideo
+            // 
+            this.txtInputVideo.AllowDrop = true;
+            this.txtInputVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtInputVideo.Location = new System.Drawing.Point(102, 9);
+            this.txtInputVideo.Name = "txtInputVideo";
+            this.txtInputVideo.Size = new System.Drawing.Size(283, 23);
+            this.txtInputVideo.TabIndex = 1;
+            this.txtInputVideo.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtInputVideo_DragDrop);
+            this.txtInputVideo.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtInputVideo_DragEnter);
+            // 
+            // btnBrowseInputVideo
+            // 
+            this.btnBrowseInputVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnBrowseInputVideo.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnBrowseInputVideo.Location = new System.Drawing.Point(391, 7);
+            this.btnBrowseInputVideo.Name = "btnBrowseInputVideo";
+            this.btnBrowseInputVideo.Size = new System.Drawing.Size(77, 25);
+            this.btnBrowseInputVideo.TabIndex = 2;
+            this.btnBrowseInputVideo.Text = "Browse...";
+            this.btnBrowseInputVideo.UseVisualStyleBackColor = true;
+            this.btnBrowseInputVideo.Click += new System.EventHandler(this.btnBrowseInputVideo_Click);
+            // 
+            // btnBrowseInputAudio
+            // 
+            this.btnBrowseInputAudio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnBrowseInputAudio.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnBrowseInputAudio.Location = new System.Drawing.Point(391, 38);
+            this.btnBrowseInputAudio.Name = "btnBrowseInputAudio";
+            this.btnBrowseInputAudio.Size = new System.Drawing.Size(77, 26);
+            this.btnBrowseInputAudio.TabIndex = 5;
+            this.btnBrowseInputAudio.Text = "Browse...";
+            this.btnBrowseInputAudio.UseVisualStyleBackColor = true;
+            this.btnBrowseInputAudio.Click += new System.EventHandler(this.btnBrowseInputAudio_Click);
+            // 
+            // txtInputAudio
+            // 
+            this.txtInputAudio.AllowDrop = true;
+            this.txtInputAudio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtInputAudio.Location = new System.Drawing.Point(102, 40);
+            this.txtInputAudio.Name = "txtInputAudio";
+            this.txtInputAudio.Size = new System.Drawing.Size(283, 23);
+            this.txtInputAudio.TabIndex = 4;
+            this.txtInputAudio.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtInputAudio_DragDrop);
+            this.txtInputAudio.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtInputAudio_DragEnter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(9, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input Audio:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnGenerate.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnGenerate.Location = new System.Drawing.Point(159, 126);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(130, 48);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtShellOutput
+            // 
+            this.txtShellOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtShellOutput.Location = new System.Drawing.Point(12, 214);
+            this.txtShellOutput.Multiline = true;
+            this.txtShellOutput.Name = "txtShellOutput";
+            this.txtShellOutput.ReadOnly = true;
+            this.txtShellOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtShellOutput.Size = new System.Drawing.Size(877, 271);
+            this.txtShellOutput.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(8, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Shell Output:";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtLog.Location = new System.Drawing.Point(474, 7);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(415, 193);
+            this.txtLog.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 497);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtShellOutput);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnBrowseInputAudio);
+            this.Controls.Add(this.txtInputAudio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowseInputVideo);
+            this.Controls.Add(this.txtInputVideo);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DINet UI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInputVideo;
+        private System.Windows.Forms.Button btnBrowseInputVideo;
+        private System.Windows.Forms.Button btnBrowseInputAudio;
+        private System.Windows.Forms.TextBox txtInputAudio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtShellOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ DINet-UI/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DINet_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void DragDrop(TextBox textbox, object sender, DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            textbox.Text = s[0];
+        }
+
+        private void txtInputVideo_DragEnter(object sender, DragEventArgs e)
+        {
+            DragEnter(sender, e);
+        }
+
+        private void txtInputVideo_DragDrop(object sender, DragEventArgs e)
+        {
+            DragDrop(txtInputVideo, sender, e);
+        }        
+
+        private void txtInputAudio_DragEnter(object sender, DragEventArgs e)
+        {
+            DragEnter(sender, e);
+        }
+
+        private void txtInputAudio_DragDrop(object sender, DragEventArgs e)
+        {
+            DragDrop(txtInputAudio, sender, e);
+        }
+
+        private void ShowFileName(TextBox textBox)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+                textBox.Text = openFileDialog1.FileName;
+        }
+
+        private void btnBrowseInputVideo_Click(object sender, EventArgs e)
+        {
+            ShowFileName(txtInputVideo);
+        }
+
+        private void btnBrowseInputAudio_Click(object sender, EventArgs e)
+        {
+            ShowFileName(txtInputAudio);
+        }
+
+        private async void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(txtInputAudio.Text) || string.IsNullOrWhiteSpace(txtInputVideo.Text))
+            {
+                MessageBox.Show("Need input audio and video");
+                return;
+            }
+            try
+            {
+                txtShellOutput.Clear();
+                txtLog.Clear();
+                btnGenerate.Enabled = false;
+                btnBrowseInputAudio.Enabled = false;
+                btnBrowseInputVideo.Enabled = false;
+                txtInputAudio.Enabled = false;
+                txtInputVideo.Enabled = false;
+
+                string condaEnv = ConfigurationManager.AppSettings["CondaEnvironmentName"];
+                string openFaceDir = ConfigurationManager.AppSettings["OpenFaceDirectory"].TrimEnd(new char[] { '\\', '/' });
+                string dinetDir = ConfigurationManager.AppSettings["DINetDirectory"].TrimEnd(new char[] { '\\', '/' });
+                string csvDir = $"{dinetDir}\\asserts\\input";
+                string vidFileWithoutExtension = Path.GetFileNameWithoutExtension(txtInputVideo.Text);
+
+                string exePath = openFaceDir + "\\featureextraction.exe";
+
+                string parameters = $"-f \"{txtInputVideo.Text}\" -out_dir \"{csvDir}\"";
+
+                string command = $"{exePath}  {parameters}";
+
+                txtLog.AppendText("Calling OpenFace to generate CSV..." + Environment.NewLine);
+
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = startInfo;
+
+                    process.Start();
+
+                    process.StandardInput.WriteLine(command);
+                    process.StandardInput.Flush();
+                    process.StandardInput.Close();
+
+                    await Task.Run(() =>
+                    {
+                        // Read and display the output of the process in real-time
+                        while (!process.StandardOutput.EndOfStream)
+                        {
+                            string outputLine = process.StandardOutput.ReadLine();
+                            Console.WriteLine("OpenFace: " + outputLine);
+
+                            txtShellOutput.Invoke((MethodInvoker)delegate
+                            {
+                                // Running on the UI thread
+                                txtShellOutput.AppendText("OpenFace: " + outputLine + Environment.NewLine);
+                            });
+                        }
+                    });
+
+                    // Check the exit code (0 indicates success)
+                    int exitCode = process.ExitCode;
+                    Console.WriteLine($"Exit Code: {exitCode}");
+                }
+                Console.WriteLine("csv created");
+                txtShellOutput.AppendText("csv created. deleting extra columns..." + Environment.NewLine);
+                txtLog.AppendText("Removing extra columns from CSV..." + Environment.NewLine);
+
+                string csvFile = csvDir + "\\" + Path.GetFileNameWithoutExtension(txtInputVideo.Text) + ".csv";
+                string inputFilePath = csvFile;
+                string outputFilePath = csvFile + ".txt";
+
+                using (var reader = new StreamReader(inputFilePath))
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (string.IsNullOrWhiteSpace(line))
+                        {
+                            continue; // Skip empty lines
+                        }
+
+                        string[] fields = line.Split(',');
+
+                        // todo: simplify to just 1 statement?
+
+                        // Filter out the columns to keep
+                        var filteredFields = fields
+                            .Select((field, index) => new { Field = field, Index = index })
+                            .Where(item => item.Index < 5 || item.Index > 298)
+                            .Select(item => item.Field)
+                            .ToArray();
+
+                        // Filter out the columns to keep
+                        var filteredFields2 = filteredFields
+                            .Select((field, index) => new { Field = field, Index = index })
+                            .Where(item => item.Index < 141 || item.Index > 420)
+                            .Select(item => item.Field)
+                            .ToArray();
+
+                        string modifiedLine2 = string.Join(",", filteredFields2);
+
+                        modifiedLine2 = modifiedLine2.Replace(",  ", ", ");
+                        writer.WriteLine(modifiedLine2);
+                    }
+                }
+
+                File.Delete(csvFile);
+                File.Move(outputFilePath, csvFile);
+
+                Console.WriteLine("Columns removed successfully.");
+                txtShellOutput.AppendText("Columns removed successfully. Starting DINet" + Environment.NewLine);
+                
+                //////////////////////////////////////////////////////////////////////////////
+                // starting DINet
+
+                // Define the Conda environment name and the Python script to execute
+                string audioFile = txtInputAudio.Text;
+                if(!audioFile.EndsWith(".wav"))
+                {
+                    txtLog.AppendText("Input audio not a wav file. converting to wav......" + Environment.NewLine);
+                    Console.WriteLine("input audio not a wav file. converting to wav...");
+                    txtShellOutput.AppendText("input audio not a wav file. converting to wav..." + Environment.NewLine);
+                    audioFile = await ConvertWithFFMPEG(audioFile, "wav");                    
+                }
+
+                string videoFile = txtInputVideo.Text;
+                if (!videoFile.EndsWith(".mp4"))
+                {
+                    txtLog.AppendText("Input video not an mp4 file. converting to mp4..." + Environment.NewLine);
+                    Console.WriteLine("input video not an mp4 file. converting to mp4...");
+                    txtShellOutput.AppendText("input video not an mp4 file. converting to mp4..." + Environment.NewLine);
+                    videoFile = await ConvertWithFFMPEG(videoFile, "mp4");                    
+                }
+
+                txtLog.AppendText("Running DINet..." + Environment.NewLine);
+
+                string pythonScriptPath = dinetDir + "\\inference.py";
+                string pythonScriptArguments = $"--mouth_region_size=256 --source_video_path=\"{videoFile}\" --source_openface_landmark_path=./asserts/input/{vidFileWithoutExtension}.csv --driving_audio_path=\"{audioFile}\" --pretrained_clip_DINet_path=./asserts/clip_training_DINet_256mouth.pth";
+
+                string scriptDirectory = Path.GetDirectoryName(pythonScriptPath);
+
+                // Construct the command to activate the Conda environment and run the Python script
+                command = $"conda activate {condaEnv} && cd {scriptDirectory} && python -u {pythonScriptPath} {pythonScriptArguments}";
+
+                // Create a process start info
+                startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = startInfo;
+
+                    // Start the process
+                    process.Start();
+
+                    // Send the command to the process
+                    process.StandardInput.WriteLine(command);
+                    process.StandardInput.Flush();
+                    process.StandardInput.Close();
+
+                    await Task.Run(() =>
+                    {
+                        // Read and display the output of the process in real-time
+                        while (!process.StandardOutput.EndOfStream)
+                        {
+                            string outputLine = process.StandardOutput.ReadLine();
+                            Console.WriteLine("DINet: " + outputLine);
+
+                            txtShellOutput.Invoke((MethodInvoker)delegate
+                            {
+                                // Running on the UI thread
+                                txtShellOutput.AppendText("DINet: " + outputLine + Environment.NewLine);
+                            });
+                        }
+                    });
+
+                    // Check the exit code (0 indicates success)
+                    int exitCode = process.ExitCode;
+                    Console.WriteLine($"Exit Code: {exitCode}");
+                }
+
+                txtLog.AppendText("DINet complete. Cleaning up files..." + Environment.NewLine);
+                // cleanup. delete directories and files that were created            
+
+                string vidFolderPath = Path.GetDirectoryName(txtInputVideo.Text) + "\\" + vidFileWithoutExtension;
+                string inferenceResultFolder = dinetDir + "\\asserts\\inference_result\\";
+                
+                Directory.Delete(vidFolderPath, true);
+                Directory.Delete(csvDir + "\\" + vidFileWithoutExtension + "_aligned", true);
+                File.Delete(csvDir + "\\" + vidFileWithoutExtension + ".avi");
+                File.Delete(csvDir + "\\" + vidFileWithoutExtension + ".csv");
+                File.Delete(csvDir + "\\" + vidFileWithoutExtension + ".hog");
+                File.Delete(csvDir + "\\" + vidFileWithoutExtension + "_of_details.txt");
+                File.Delete(inferenceResultFolder + vidFileWithoutExtension + "_facial_dubbing.mp4");
+                File.Delete(inferenceResultFolder + vidFileWithoutExtension + "_synthetic_face.mp4");
+                
+                txtShellOutput.AppendText("Process Complete");
+                txtLog.AppendText("Process Complete");
+                Process.Start(inferenceResultFolder + vidFileWithoutExtension + "_facial_dubbing_add_audio.mp4");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Something went wrong. Check shell output for details.");
+                txtShellOutput.AppendText("Message: " + ex.Message + Environment.NewLine);
+                txtShellOutput.AppendText("Stack Trace: " + ex.StackTrace + Environment.NewLine);
+            }
+            finally
+            {
+                btnGenerate.Enabled = true;
+                btnBrowseInputAudio.Enabled = true;
+                btnBrowseInputVideo.Enabled = true;
+                txtInputAudio.Enabled = true;
+                txtInputVideo.Enabled = true;
+            }
+        }
+
+        private async Task<string> ConvertWithFFMPEG(string inputFile, string format)
+        {
+            string filenameNoExt = Path.GetFileNameWithoutExtension(inputFile);
+            string directory = Path.GetDirectoryName(inputFile);
+            string newFileName = directory + "\\" + filenameNoExt + "." + format;
+            string imageParams = "";
+            if (inputFile.EndsWith(".jpg") || inputFile.EndsWith(".jpeg") || inputFile.EndsWith(".png"))
+                imageParams = " -vcodec libx264 -vf \"pad=ceil(iw/2)*2:ceil(ih/2)*2\" -r 24 -y -an";
+
+            string parameters = $"-i \"{inputFile}\"{imageParams} \"{newFileName}\"";
+
+            string command = $"ffmpeg {parameters}";
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+
+                process.Start();
+
+                process.StandardInput.WriteLine(command);
+                process.StandardInput.Flush();
+                process.StandardInput.Close();
+
+                await Task.Run(() =>
+                {
+                    // Read and display the output of the process in real-time
+                    while (!process.StandardOutput.EndOfStream)
+                    {
+                        string outputLine = process.StandardOutput.ReadLine();
+                        Console.WriteLine("ffmpeg: " + outputLine);
+
+                        txtShellOutput.Invoke((MethodInvoker)delegate
+                        {
+                            // Running on the UI thread
+                            txtShellOutput.AppendText("ffmpeg: " + outputLine + Environment.NewLine);
+                        });
+                    }
+                });
+
+                // Check the exit code (0 indicates success)
+                int exitCode = process.ExitCode;
+                Console.WriteLine($"Exit Code: {exitCode}");
+            }
+
+            return newFileName;
+        }
+    }
+}
